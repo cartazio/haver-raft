401ada6813927ca0c1fd97c23acefd32bedda653
buckie <wjmartino@gmail.com>
2015-08-08 16:44:44 -0400

    mostly formatting help
    Signed-off-by: buckie <wjmartino@gmail.com>

--- Raft.hs
+++ Raft.hs
@@ -24,53 +24,60 @@ data Output = Output deriving (Eq,Ord,Show)
 nodes :: Set Name
 nodes = undefined
 
-data Entry = Entry { eAt :: Name
-                    ,eClient :: Natural
-                    ,eId :: LogIndex
-                    ,eIndex :: LogIndex
-                    ,eTerm :: Term
-                    ,eInput :: Input
-                    } deriving (Eq,Ord,Show )
+data Entry = Entry
+    {eAt :: Name
+    ,eClient :: Natural
+    ,eId :: LogIndex
+    ,eIndex :: LogIndex
+    ,eTerm :: Term
+    ,eInput :: Input
+    } deriving (Eq,Ord,Show)
 
 data Msg = RequestVote Term Name LogIndex Term
-          | RequestVoteReply Term Bool
-          | AppendEntries Term Name LogIndex Term [Entry] LogIndex
-          | AppendEntriesReply
-          deriving (Eq,Ord,Show)
+         | RequestVoteReply Term Bool
+         | AppendEntries Term Name LogIndex Term [Entry] LogIndex
+         | AppendEntriesReply
+         deriving (Eq,Ord,Show)
 
+data RaftInput = Timeout
+               | ClientRequest Natural Natural Input
+               deriving (Eq,Ord,Show)
 
-data RaftInput = Timeout | ClientRequest Natural Natural Input
-    deriving (Eq,Ord,Show)
+data RaftOut = NotLeader Natural Natural
+             | ClientResponse Natural Natural Output
+             deriving (Eq,Ord,Show)
 
-data RaftOut = NotLeader  Natural Natural
-        | ClientResponse Natural Natural Output
-        deriving (Eq,Ord,Show)
-
-data ServerType = Follower | Candidate | Leader
-  deriving (Eq,Ord,Show)
-
+data ServerType = Follower
+                | Candidate
+                | Leader
+                deriving (Eq,Ord,Show)
 
 findAtIndex :: [Entry] -> LogIndex -> Maybe Entry
 findAtIndex [] _ = Nothing
-findAtIndex (e:es)  i  | eIndex e ==  i = Just e
-                       | eIndex e  < i = Nothing
-                       | otherwise = findAtIndex es i
+findAtIndex (e:es) i
+  | eIndex e ==  i = Just e
+  | eIndex e  < i = Nothing
+  | otherwise = findAtIndex es i
 
 findGtIndex :: [Entry] -> LogIndex -> [Entry]
-findGtIndex [] i = []
+findGtIndex [] _ = []
 findGtIndex (e:es) i | eIndex e > i = e : findGtIndex es i
                      | otherwise = []
 
-removeAfterIndex :: [Entry] -> LogIndex  -> [Entry]
-removeAfterIndex  = undefined
+removeAfterIndex :: [Entry] -> LogIndex -> [Entry]
+removeAfterIndex [] _ = []
+removeAfterIndex logs@(e:es) i
+  | eIndex e <= i = logs
+  | otherwise = removeAfterIndex es i
 
 maxIndex :: [Entry] -> LogIndex
 maxIndex  [] = LogIndex 0
-maxIndex (e:es) = eIndex e
+maxIndex (e:_) = eIndex e
 
 maxTerm :: [Entry] -> Term
 maxTerm [] = Term 0
-maxTerm  (e:es) =  eTerm e
+maxTerm  (e:_) =  eTerm e
+
 
 
 
