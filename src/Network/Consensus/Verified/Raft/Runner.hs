5bccd0441f7a8d902f14c920d21a3d6f296a5fb9
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2015-08-19 23:21:33 -0400

    insanely abstracted "free" style initialization code for basic runners for raft

--- src/Network/Consensus/Verified/Raft/Runner.hs
+++ src/Network/Consensus/Verified/Raft/Runner.hs
@@ -17,15 +17,19 @@ import Network.Consensus.Verified.Raft (Res(..)
 import Data.Map (Map)
 import Data.Set (Set)
 import Data.Reflection
-
-
+import qualified Data.Map as Map
+import Control.Applicative(Alternative,(<|>))
 
 data Arrangement m name state input output message request_id =
   Arrangement {
         init :: name -> state
         ,reboot :: state -> state
+        -- note : handleIO and handleNet
+        -- both asssume state machines do stateful update
+        -- afaik, wrt extraction
         ,handleIO :: name -> input -> state ->  m (Res output state name message)
         ,handleNet :: name -> name -> message -> state -> m (Res output state name message)
+        ,handleTimeout :: name -> state -> m (Res output state name message)
         ,setTimeout :: name -> state ->  Res output state name message
         ,deserialize ::  String -> Maybe (request_id,input)
         ,serialize :: output -> (request_id,String)
@@ -53,13 +57,14 @@ defaultArrangement ps pk  = Arrangement {
     ,reboot = Raft.reboot
     ,handleIO = Raft.raftInputHandler ps pk
     ,handleNet = Raft.raftNetHandler ps pk
+    ,handleTimeout = (\nm st -> return $ Raft.handleTimeout pk nm st)
     ,setTimeout = Raft.handleTimeout pk
-    ,deserialize = undefined
-    ,serialize = undefined
+    ,deserialize = error "undefined deserialize Arrangement"
+    ,serialize = error "undefined serialize Arrangement"
     ,debug = False
-    ,debugRecv = undefined
-    ,debugSend = undefined
-    ,debugTimeout = undefined
+    ,debugRecv = error "undefined debugRecv"
+    ,debugSend = error "undefined debugSend"
+    ,debugTimeout = error "undefined debugTimeout"
   }
 
 
@@ -77,11 +82,136 @@ data Env m  state out_channel file_descr request_id sockaddr name = Env {
     ,outstandingUpdate ::  Map request_id file_descr -> m ()
     ,savesRead :: m Int -- think IORef Int
     ,savesWrite :: Int -> m ()
-    ,nodes :: m [(name,sockaddr)] -- m [...] to model membership list may change.
+    ,nodes :: m (Map name sockaddr) -- m [...] to model membership list may change.
                                   -- though verdi proof assumes fixed
+
   }
 
 
 data LogStep name msg input = LogInput input | LogNet name msg  | LogTimeout
   deriving (Eq,Ord,Show)
+
+
+data EnvOps m out_channel state file_descr sockaddr name msg input  = EnvOps {
+    -- yieldLogEvents assumes the only newlines are betwee
+    -- log events
+    -- yields nothing when reaches end of file
+    open :: String -> m file_descr
+    ,yieldLogEvents ::  file_descr -> m (Maybe (LogStep name msg input))
+    ,loadSnapShot :: String -> m state
+  }
+
+update_state_from_log_entry ::
+  forall
+    b
+    f
+    output
+    name
+    msg
+    input
+    request_id.
+         Functor f
+         => Arrangement f name b input output msg request_id
+         -> name -> b -> LogStep name msg input -> f b
+update_state_from_log_entry env nm s op  =
+    ((\(_,st,_)-> st) . unRes) <$> case op of
+        LogInput inp  ->  (handleIO env) nm inp s
+        LogNet src m -> (handleNet env) nm src m s
+        LogTimeout -> (handleTimeout env) nm s
+
+get_initial_state :: forall
+            f   -- (f :: * -> *)
+            out_channel
+            file_descr
+            sockaddr
+            state
+            name
+            msg
+            input
+            m
+            --(m :: * -> *)
+
+            output
+            message
+            request_id .
+     Alternative f =>
+     EnvOps f out_channel state file_descr sockaddr name msg input
+     -> Arrangement m name state input output message request_id
+     -> String
+     -> name
+     -> f state
+get_initial_state eop arr snpfile nm =
+    (loadSnapShot eop) snpfile <|>  (pure $ (init arr) nm )
+
+
+
+
+restore_from_log :: forall
+           m -- (m :: * -> *)
+           out_channel
+           state
+           file_descr
+           sockaddr
+           name
+           msg
+           input
+           output
+           request_id.
+    Monad m =>
+    Arrangement m name state input output msg request_id
+    -> EnvOps m out_channel state file_descr sockaddr name msg input
+    -> file_descr
+    -> name
+    -> state
+    -> m state
+restore_from_log env eop fd nm st = go st
+  where
+    yielder = (yieldLogEvents eop) fd
+    go theState = do
+      nextOp <- yielder
+      case nextOp of
+          Nothing -> return theState
+          Just op ->
+           do st' <-
+                update_state_from_log_entry env nm st op
+              go st'
+
+restore :: forall  m -- (m :: * -> *)
+                        state
+                         -- (m1 :: * -> *)
+                        output
+                        message
+                        request_id
+                        out_channel
+                        file_descr
+                        sockaddr
+                        name
+                        msg
+                        input .
+                 (Monad m, Alternative m) =>
+                 Arrangement m name state input output message request_id
+                 -> Arrangement m name state input output msg request_id
+                 -> EnvOps m out_channel state file_descr sockaddr name msg input
+                 -> String
+                 -> String
+                 -> name
+                 -> m state
+restore arr env eop snpfile logfl nm =
+   do
+    -- this doesn't deal with catching up with
+    -- nonlocal update ... i think....
+     istate <- get_initial_state eop arr snpfile nm
+     logfd <- (open eop) logfl
+     restore_from_log env eop logfd nm istate
+
+
+denote :: (Monad m,Ord name)
+   => Env m  state out_channel file_descr request_id sockaddr name
+   -> name -> m sockaddr
+denote env nm =
+  do  nds <- nodes env
+      maybe
+        (fail "bad lookup with denote")
+        return
+        $  Map.lookup  nm nds
 
