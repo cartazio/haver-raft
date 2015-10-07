b0b97f001a50ed5d3a1fd3a5930a41b0b11aa843
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2015-10-07 19:54:12 -0400

    covers all three basic nice things

--- tests/Framing.hs
+++ tests/Framing.hs
@@ -4,12 +4,26 @@ import Network.Message.Framing
 import Test.Hspec
 import qualified Data.ByteString.Lazy.Char8 as LBSC
 import Data.Monoid((<>))
-
+import Data.ByteString.Builder as DBB
 main :: IO ()
 main = hspec $ do
-  it "handles split messages" $
+  it "handles multiple messages" $
     let x =  (Message$ LBSC.pack "hello dotty!")
         y =  (Message$ LBSC.pack "whats up doc")
       in
           [x,y] `shouldBe`  (either (error "fail")  id $ prependAndGetMessage emptyResidue (putMsg x <> putMsg y ))
+  it "handles split messages" $
+      let  z = (Message $ LBSC.pack "hello dotty!")
+           x = putMsg z
+           x' = Residue $ DBB.lazyByteString $  LBSC.take 3 x
+           y' =  LBSC.drop 3 x
+        in [z] `shouldBe` (either (error "split fail") id $ prependAndGetMessage x' y' )
+  it "handles prefixes" $
+      let  z = (Message $ LBSC.pack "hello dotty!")
+           x = putMsg z
+           x' =  LBSC.take 3 x
+           --y' =  LBSC.drop 3 x
+        in x' `shouldBe` ( DBB.toLazyByteString $ prefix $ either snd (error "prefix fail ")
+                          $ prependAndGetMessage emptyResidue x')
+
 
